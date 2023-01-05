e6a8694889df14fa61632e43443c80c880cfe4ac
Jeffrey <jeffrey.graham600@gmail.com>
2023-01-05 02:01:26 -0500

    finished work component

---
+++ src/components/data/data.js
@@ -0,0 +1,32 @@
+import MemeGen from "../assets/projects/memeimg.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: MemeGen,
+    github: "",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: MemeGen,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: MemeGen,
+    github: "",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: MemeGen,
+    github: "",
+    live: "",
+  },
+];
