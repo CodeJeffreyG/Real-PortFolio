5e838575c731d4a0460645c0bcc3e30e33177c2c
Jeffrey <jeffrey.graham600@gmail.com>
2023-01-04 23:06:18 -0500

    finished home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,30 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f] ">
       {/* container */}
+
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-2xl text-[#597891]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Jeffrey Graham
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Front-End Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a Front-End developer specializing in the React.js framework
+          focused on creating an exceptional digital experience. Currently I am
+          focused on building responsive Front-End Applications.
+        </p>
+
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ccd6f6] hover:text-[black] hover: border-[#ccd6f6]">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-4" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
