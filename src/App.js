8b11315b6b4da9131908b46f453e497c0cf0ed8b
Jeffrey <jeffrey.graham600@gmail.com>
2023-01-04 23:17:11 -0500

    starting about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
